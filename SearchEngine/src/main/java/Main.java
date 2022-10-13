02dee776ae2cb62a11909f3b1fd6409cffa38ede
javalinks <dtunev@mail.ru>
2022-10-13 12:59:38 +0500

    search_engine - stages 3,4, modified main.class

--- SearchEngine/src/main/java/Main.java
+++ SearchEngine/src/main/java/Main.java
@@ -1,11 +1,10 @@
+import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.concurrent.ForkJoinPool;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, IOException {
 
         System.setProperty("java.net.useSystemProxies", "true");
 
@@ -14,36 +13,19 @@ public class Main {
         String url = scanner.nextLine();
         scanner.close();
 
-        SiteMap siteMap = new SiteMap(url);
-        Map<String, Map<String, Integer>> dataToPage = new ForkJoinPool().invoke(siteMap);
+        DataForDBTables dataForTables = new DataForDBTables(url);
+        dataForTables.setsiteMapWithContent();
 
-        System.out.println("Количество элементов = " + dataToPage.size());
+        DBConnection dbConnection = new DBConnection();
 
-        DBConnection.getConnection();
-        DBConnection.createTablePage();
-        dataToPageTable(dataToPage);
-        DBConnection.insertIntoPage();
-    }
-
-    public static String SubString(String link) {
-        return link.replaceFirst("http(s)?://(www\\.)?\\w+\\.\\w+",
-                "");
-    }
-
-    public static void dataToPageTable(Map<String, Map<String, Integer>> dataToPage) throws SQLException {
-        Map<String, Integer> contentAndStatusCode;
-
-        for (Map.Entry<String, Map<String, Integer>> item : dataToPage.entrySet()) {
-            String link = SubString(item.getKey());
-            contentAndStatusCode = item.getValue();
-            int statusCode = 0;
-            String content = null;
-            for (Map.Entry<String, Integer> contentItem : contentAndStatusCode.entrySet()) {
-                content = contentItem.getKey().replaceAll("'+|,+", "");
-                statusCode = contentItem.getValue();
-            }
-            DBConnection.dataToPageTable(link, statusCode, content);
-            contentAndStatusCode.clear();
-        }
+        dbConnection.getConnection();
+        dbConnection.createTableField();
+        dbConnection.insertIntoField();
+        dbConnection.createTableLemma();
+        dbConnection.createTablePage();
+        dataForTables.dataToTables(dataForTables.getsiteMapWithContent());
+        dbConnection.createTableIndex();
+        dataForTables.dataToIndexTable();
+        dbConnection.insertIntoIndex();
     }
 }
